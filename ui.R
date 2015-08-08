7f82164369ee0d40e061f4bedbf8d154ee9c17dd
eugeneleefl <eugeneleefl@gmail.com>
2015-08-09 00:41:37 +0800

    Edits
    Edits

--- ui.R
+++ ui.R
@@ -12,7 +12,7 @@ shinyUI(fluidPage(
                   "Number of Means Simulated",
                   min = 10,
                   max = 500,
-                  value = 10,
+                  value = 20,
                   animate=TRUE),
     
       sliderInput("rate",
@@ -23,5 +23,6 @@ shinyUI(fluidPage(
     mainPanel(
       plotOutput("distPlot"),
       h4('Theorectical Mean'),
-      verbatimTextOutput("inputValue")
+      verbatimTextOutput("inputValue"),
+      p('Adjust the sliders to create the cumulative means for exponential random variables with n = 40. You can adjust the rate and the number of means to be generated in the simulation. Notice that as the number of means increases, the cumulative mean tends towards the theoretical mean. This demonstrates Central Limit Theorem. This application is created using code I created from the Coursera Statistical Inference course project as part of the Data Science specialisation.')
     ))))
